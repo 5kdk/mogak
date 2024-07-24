c5c01464dcb29e75a83e78d0bf6e856fffdc40b5
5kdk <5kdk.code@gmail.com>
2024-07-25 00:47:17 +0900

    fix: css variable, font config

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,13 @@
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
+import { Gothic_A1 } from 'next/font/google';
 import './globals.css';
+import { cn } from '@/src/lib/utils';
 
-const inter = Inter({ subsets: ['latin'] });
+const gothicA1 = Gothic_A1({
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+  subsets: ['latin'],
+  variable: '--font-gothic-a1',
+});
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -15,8 +20,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="ko">
+      <body className={cn('min-h-screen bg-background font-sans antialiased', gothicA1.variable)}>
+        {children}
+      </body>
     </html>
   );
 }
